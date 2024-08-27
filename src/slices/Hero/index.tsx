a19a967f27222f6c51949b936586cc7f99ebbd69
kranti99 <krantitherevolution99@gmail.com>
2024-08-27 08:41:31 +0545

    Update index.tsx

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -83,10 +83,29 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             }
           >
             <span className="block text-slate-300 ">
-              Kantiman
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">K</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">n</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">t</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">i</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">m</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
+              <span class="name-animation name-animation-first inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">n</span>
+              
             </span>
             <span className="-mt-[.2em] block text-slate-500  ">
-                Bajracharya
+      
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">B</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">j</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">r</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">c</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">h</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">r</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">y</span>
+              <span class="name-animation name-animation-last inline-block opacity-0" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px); transform-origin: 0% 0%;">a</span>
             </span>
           </h1>
           <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
